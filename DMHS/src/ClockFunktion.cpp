995233a0a762cc066b1b736eebd6e236b17889dc
davidP1278 <david.provenzano@edu.gbssg.ch>
2024-05-30 09:52:51 +0200

    clockFunktion

--- DMHS/src/ClockFunktion.cpp
+++ DMHS/src/ClockFunktion.cpp
@@ -1,5 +1,8 @@
 #include <Arduino.h>
+#include <ESP32Time.h>
 
+
+ESP32Time rtc(3600);  
 
 void setup() {
 
@@ -7,4 +10,20 @@ void setup() {
 
 void loop() {
 
+String zeit1 = rtc.getTime("%H:%M");
+int i = 0;
+
+    while (i = 1)
+    {
+        String zeit2 = rtc.getTime("%H:%M");
+
+        if(zeit1 =! zeit2)
+        {
+            Serial.println(rtc.getTime("%H:%M"));
+            i++;
+
+            delay(1000);
+            
+        }
+    }
 }
