b0e097712827bb6eb42c6bd927346d16247df929
davidP1278 <david.provenzano@edu.gbssg.ch>
2024-05-30 15:27:32 +0200

    hi

--- DMHS/src/Clock/ClockProvider.cpp
+++ DMHS/src/Clock/ClockProvider.cpp
@@ -4,6 +4,27 @@
 #include <ESP32Time.h>
 
 static ESP32Time rtc;  
+/**
+ * \brief   Initializes the clock access module
+ * 
+ * \param[in]   offset  [s] Number of seconds the timezone is away from UTC
+ */
+void CP_init(int offset) 
+{
+    rtc.offset = offset;
+}
+
+/**
+ * \brief   Formats the current hour as string
+ * 
+ * \return  String with the current hour as string formatted number
+ */
+String CP_getHourAsString()
+{
+    // Serial.println(rtc.getTime("%d.%m.%Y %H:%M:%S"));
+    return rtc.getTime("%d.%m.%Y %H:%M:%S");
+} 
+
 /*static void CP_loop() {
 
     String time1 = rtc.getTime("%H:%M");
@@ -24,23 +45,3 @@ static ESP32Time rtc;
     }
 }
 */
-/**
- * \brief   Initializes the clock access module
- * 
- * \param[in]   offset  [s] Number of seconds the timezone is away from UTC
- */
-void CP_init(int offset) 
-{
-    rtc.offset = offset;
-}
-
-/**
- * \brief   Formats the current hour as string
- * 
- * \return  String with the current hour as string formatted number
- */
-String CP_getHourAsString()
-{
-    Serial.println(rtc.getTime("%d.%m.%Y %H:%M:%S"));
-    return rtc.getTime("%H");
-} 
