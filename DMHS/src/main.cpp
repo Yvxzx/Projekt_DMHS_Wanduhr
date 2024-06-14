1ece3257f10ad5f01da3fc3559592bee9fdad38a
davidP1278 <david.provenzano@edu.gbssg.ch>
2024-06-14 08:52:07 +0200

    vieles

--- DMHS/src/main.cpp
+++ DMHS/src/main.cpp
@@ -15,3 +15,20 @@ void loop()
   delay(60000);
 }
 
+/*
+// Buchstaben-Jagd IMS-T
+int number1 = 2;
+int number2 = 3;
+int solution = 0;
+
+solution = number1 + number2;
+
+if(solution != 5)
+{
+  Serial.println("Number 7 at position 6");
+}
+else
+{
+  Serial.println("Number 8 at position 6");
+}
+*/
